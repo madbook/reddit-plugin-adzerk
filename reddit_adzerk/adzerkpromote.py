482e3fed0f64c10eb4e1e963daacd25b43cdce97
David Wick <wick.d.spencer@gmail.com>
2016-09-20 15:52:31 -0700

    Revert "Add `adblock` property for targeting adblock users"
    This reverts commit fd677c6008d9f75cdf871fed972f9376da5d29fd.

--- reddit_adzerk/adzerkpromote.py
+++ reddit_adzerk/adzerkpromote.py
@@ -1302,10 +1302,6 @@ class AdzerkApiController(api.ApiController):
         referrer=VPrintable("referrer", max_length=2048),
         user_day=VInt("day", 0),
         user_hour=VInt("hour", 0),
-        adblock=VOneOf("adblock", [
-            "installed",
-            "off",
-        ], default=None),
     )
     def POST_request_promo(
         self,
@@ -1341,9 +1337,6 @@ class AdzerkApiController(api.ApiController):
         properties = properties_from_context(c, site)
         properties['user_day'] = user_day
         properties['user_hour'] = user_hour
-
-        if adblock is not None:
-            properties["adblock"] = adblock
 
         # backwards compat
         if platform is None:
