06b4a5f7d6f3f94cd51e43b322b898fa35555820
David Wick <wick.d.spencer@gmail.com>
2016-10-04 17:39:04 -0700

    Pass priorities to `add_tracker`

--- reddit_adzerk/adzerkpromote.py
+++ reddit_adzerk/adzerkpromote.py
@@ -1394,13 +1394,16 @@ class AdzerkApiController(api.ApiController):
 
         res_by_campaign = {r.campaign: r for r in response}
         adserver_click_urls = {r.campaign: r.click_url for r in response}
+        priorities = {r.campaign: r.priority for r in response}
         tuples = [promote.PromoTuple(r.link, 1., r.campaign) for r in response]
         builder = CampaignBuilder(tuples, wrap=default_thing_wrapper(),
                                   keep_fn=promote.promo_keep_fn,
                                   num=1,
                                   skip=True)
         listing = LinkListing(builder, nextprev=False).listing()
-        promote.add_trackers(listing.things, c.site, adserver_click_urls=adserver_click_urls)
+        promote.add_trackers(
+            listing.things, c.site,
+            adserver_click_urls=adserver_click_urls, priorities=priorities)
         promote.update_served(listing.things)
         if listing.things:
             g.stats.simple_event('adzerk.request.valid_promo')
